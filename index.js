648d65277bf3d09a85e08d53397af016f6539380
Aidan Duggan <aduggan@tcd.ie>
2018-11-18 11:07:02 +0000

    base

---
+++ index.js
@@ -0,0 +1,38 @@
+const yaml = require('js-yaml')
+const ignore = require('ignore')
+
+module.exports = app => {
+  app.on('pull_request.opened', autolabel)
+  app.on('pull_request.synchronize', autolabel)
+
+  async function autolabel (context) {
+    const content = await context.github.repos.getContent(context.repo({
+      path: '.github/autolabeler.yml'
+    }))
+    const config = yaml.safeLoad(Buffer.from(content.data.content, 'base64').toString())
+
+    const files = await context.github.pullRequests.getFiles(context.issue())
+    const changedFiles = files.data.map(file => file.filename)
+
+    const labels = new Set()
+
+    // eslint-disable-next-line guard-for-in
+    for (const label in config) {
+      app.log('looking for changes', label, config[label])
+      const matcher = ignore().add(config[label])
+
+      if (changedFiles.find(file => matcher.ignores(file))) {
+        labels.add(label)
+      }
+    }
+
+    const labelsToAdd = Array.from(labels)
+
+    app.log('Adding labels', labelsToAdd)
+    if (labelsToAdd.length > 0) {
+      return context.github.issues.addLabels(context.issue({
+        labels: labelsToAdd
+      }))
+    }
+  }
+}
